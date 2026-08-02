2954390846e8a2bb99252195f418d64f69e6de5b
Caleb Baraka <caleb.blessings@student.moringaschool.com>
2026-08-02 21:57:03 +0300

    improved styling of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import "./styles/App.css";
 
-import Header from "./components/Header";
-import ProjectForm from "./components/ProjectForm";
-import SearchBar from "./components/SearchBar";
-import ProjectList from "./components/ProjectList";
+import Header from "./components/header";
+import ProjectForm from "./components/projectform";
+import SearchBar from "./components/searchbar";
+import ProjectList from "./components/projectlist";
 
 function App() {
 
